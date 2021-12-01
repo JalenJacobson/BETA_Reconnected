596958db5676b089fd4fb03be4fc6e160dfab067
jacobsonj <justinjacobson0118@gmail.com>
2021-12-01 06:54:08 -0700

    tutorial text

---
+++ Assets/Scripts/General/Tutorial_Cube.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial_Cube : MonoBehaviour
+{
+    public Text tutorialText;
+    public string changeTextTo;
+    public string objectThatShouldTrigger;
+    public bool displayTextOnlyOnce;
+    private bool alreadyDisplayed = false;
+
+    void Start()
+    {
+        tutorialText = GameObject.Find("Tutorial").GetComponent<Text>();;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.name.Contains(objectThatShouldTrigger))
+        {
+            if(displayTextOnlyOnce)
+            {
+                if(alreadyDisplayed)
+                {
+                    return;
+                }
+                else {changeText();}
+            }
+            else changeText();
+        }
+        
+    }
+
+    public void changeText()
+    {
+        tutorialText.text = changeTextTo;
+        alreadyDisplayed = true;
+    }
+}
