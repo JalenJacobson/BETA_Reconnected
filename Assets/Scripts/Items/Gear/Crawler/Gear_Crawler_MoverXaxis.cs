0b1bb6668637f8a97a5396bf605ab6d767aff0b5
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2021-12-11 15:40:11 -0700

    LuzGears

--- Assets/Scripts/Items/Gear/Crawler/Gear_Crawler_MoverXaxis.cs
+++ Assets/Scripts/Items/Gear/Crawler/Gear_Crawler_MoverXaxis.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class Gear_Crawler_MoverXaxis : MonoBehaviour
 {
     public GameObject touching = null;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (touching == true)
+        {
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown("d"))
+             {
+                 anim.Play("Forward");
+             }
+
+            if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp("d"))
+             {
+                 anim.Play("Stop");
+             }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown("a"))
+             {
+                 anim.Play("Backward");
+             }
+
+            if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp("a"))
+             {
+                 anim.Play("Stop");
+             }
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -26,18 +49,23 @@ public class Gear_Crawler_MoverXaxis : MonoBehaviour
         {
              touching = other.gameObject;
              touching.SendMessage("toggleX");
+
+
+
         }
+
     
     } 
     void OnTriggerExit(Collider other)
     {
-        //   anim.Play("Stop");
+        anim.Play("Stop");
         print(other.name);
         if(other.name.Contains("Stand"))
         {
              touching = other.gameObject;
              touching.SendMessage("toggleX");
         }
+        touching = null;
     
     } 
 }
--- Assets/Scripts/Items/Gear/Crawler/Gear_Crawler_Mover_Zaxis.cs
+++ Assets/Scripts/Items/Gear/Crawler/Gear_Crawler_Mover_Zaxis.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class Gear_Crawler_Mover_Zaxis : MonoBehaviour
 {
     public GameObject touching = null;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+               if (touching == true)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w"))
+             {
+                 anim.Play("Forward");
+             }
+
+            if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp("w"))
+             {
+                 anim.Play("Stop");
+             }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s"))
+             {
+                 anim.Play("Backward");
+             }
+
+            if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp("s"))
+             {
+                 anim.Play("Stop");
+             }
+        } 
     }
 
     void OnTriggerEnter(Collider other)
@@ -31,6 +53,7 @@ public class Gear_Crawler_Mover_Zaxis : MonoBehaviour
     } 
     void OnTriggerExit(Collider other)
     {
+        touching = null;
         //   anim.Play("Stop");
         print(other.name);
         if(other.name.Contains("Stand"))
@@ -38,6 +61,7 @@ public class Gear_Crawler_Mover_Zaxis : MonoBehaviour
              touching = other.gameObject;
              touching.SendMessage("toggleZ");
         }
+        touching = null;
     
     } 
 }
