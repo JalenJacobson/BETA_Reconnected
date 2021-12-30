8df3c74ecd77b075aeff1c33eba3fcbeb2b0e240
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2021-12-30 14:56:35 -0700

    level select screen

--- Assets/Scripts/Level_Management/MoveNode.cs
+++ Assets/Scripts/Level_Management/MoveNode.cs
@@ -3,33 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MoveNode : Player
+public class MoveNode : MonoBehaviour
 {
-    // public float moveSpeed = 10;
-    // public float rotateSpeed = 10;
-    // public Rigidbody rb;
-    // public bool toggleSelected;
-    // public Vector3 direction;
-    // public bool fixPosition = false;
-    // public Vector3 startPos;
-    // public float breathRemaining = 5f;
-    // public bool touchingAirBubble = false;
-    // public bool inWater = false;
-    // public Text DangerField;
-    // public Text DangerState;
-    // public Color orangeGravityField;
-    // public Color greenConsole;
-    // public Color blueCircuitField;
-    // public Color redDanger;
-
-
-    // public Joystick joystick;
+    public float moveSpeed = 10;
+    public float rotateSpeed = 10;
+    public Vector3 directionRotate;
+    public Vector3 directionMove;
+    public string moveAxisHorizontal;
+    public string moveAxisVertical;
+    public string playerNumber;
+    public Joystick joystick;
+    public Rigidbody rb;
 
     virtual public void Start()
     {
-        name = "Node";
-        startPos = new Vector3(-1482.761f, 81f, -1024.285f);
-        transform.position = startPos;
+
+    }
+
+    void Joystick()
+    {
+        moveAxisHorizontal = "joystick.Horizontal";
+        moveAxisVertical = "joystick.Vertical";
+    }
+
+    public void Movement()
+    {
+        float horizontalMove = Input.GetAxis(moveAxisHorizontal);
+        float verticalMove = Input.GetAxis(moveAxisVertical);
+
+        directionRotate = new Vector3(horizontalMove, 0.0f, verticalMove);
+        directionMove = new Vector3(horizontalMove * moveSpeed, rb.velocity.y, verticalMove * moveSpeed);
+        rb.velocity = directionMove;
+
+
+
+        // rb.MovePosition(transform.position + moveSpeed * Time.deltaTime * direction);
+        
     }
 
     
