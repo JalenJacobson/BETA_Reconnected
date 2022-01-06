eb0b0315a214f7ecfce3b48d872fb16a2ebd274b
JalenJacobson <74105092+JalenJacobson@users.noreply.github.com>
2022-01-06 12:08:57 -0700

    Redo Stuff

--- Assets/StartPosReset.cs
+++ Assets/StartPosReset.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class StartPosReset : MonoBehaviour
 {
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+      anim = GetComponent<Animator>();   
     }
 
     // Update is called once per frame
@@ -22,6 +23,7 @@ public class StartPosReset : MonoBehaviour
         if(characterName == "IdleLuz" || characterName == "Gears" || characterName == "SatBot" || characterName == "Pump" || characterName == "Brute")
         {
             other.gameObject.SendMessage("changeStartPos", transform.position);
+            anim.Play("RestartStickOn");
         }
     }
 }
